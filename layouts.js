80b39b5fd20828f4939696a0d7341ef469e381c4
dageweb <dageweb@proton.me>
2025-05-17 00:21:32 -0400

    Update layouts.js

--- layouts.js
+++ layouts.js
@@ -6,6 +6,7 @@ function switchLanguage() {
   if (!newLang) return;
 
   document.cookie = `language=${newLang}; path=/; max-age=${60 * 60 * 24 * 365}`;
+  language = newLang;
   loadPublicComponents();
 
   if (eventLanguageChanged !== null) {
